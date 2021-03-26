c43c5180b36bd9adffc88a3f31058aff607a8f15
Ahmed Mohamed <Ahmed.Viper@outlook.com>
2021-03-26 16:52:56 +0200

    Updated

---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookle_2._0.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
